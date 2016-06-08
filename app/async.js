c614cdfa24844bf19ff6a1487c6365d1611a088f
Tasio Victoria <tasiovictoria@gmail.com>
2016-06-08 17:28:09 -0400

    resolves failing tests in async.js

--- app/async.js
+++ app/async.js
@@ -2,10 +2,19 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    return new Promise(function(resolve) {
+      resolve(value);
+    });
   },
 
   manipulateRemoteData: function(url) {
-
+    return new Promise(function(resolve, reject) {
+      $.get('http://localhost:4444' + url, function(data) {
+        var nameData = _.reduce(data.people, function(memo, obj) {
+          return memo.concat(obj.name);
+        }, []);
+        resolve(_.sortBy(nameData));
+      });
+    });
   }
 };
