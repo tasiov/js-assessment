f6a39094bc7c9905401fcaddd8977f2b38e24e91
Tasio Victoria <tasiovictoria@gmail.com>
2016-06-08 17:26:35 -0400

    resolves failing tests in objects.js

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,16 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   alterObjects: function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
-
+    return _.reduce(Object.keys(obj), function(memo, key) {
+      return memo.concat(key + ": " + obj[key]);
+    }, []);
   }
 };
